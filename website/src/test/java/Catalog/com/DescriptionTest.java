cd0af730cdc18abdb7f166feaeb514d2c24cec70
stepawka1 <56771735+stepawka1@users.noreply.github.com>
2020-01-09 16:37:10 +0300

    Update DescriptionTest.java

--- website/src/test/java/Catalog/com/DescriptionTest.java
+++ website/src/test/java/Catalog/com/DescriptionTest.java
@@ -7,24 +7,30 @@ import java.util.*;
 import org.junit.Test;
 
 import Catalog.com.Description;
-
-public class DescriptionTest
+import Catalog.com.Purchase;
+public class ModelsTest
 {
     private ArrayList<String> t = new ArrayList<>();
 	private Description model = new Description("hello", 2, 3000,t ,"stepa","path",4);
+	private Purchase p = new Purchase("a",21000,5);
 
     @Test
-    public void getModelPrice() {
+    public void getMDescriptionPrice() {
         assertEquals(3000, model.getPrice()); 
     }
     @Test
-    public void getModelfind() {
-        assertEquals(true, model.find(4)); 
+    public void getDescriptionfind() {
+        assertEquals(true, model.find(4));
     }
     @Test
-    public void getModelQ() {
+    public void getDescriptionQ() {
         assertEquals(2, model.getQuintity()); 
     }
-
+    @Test
+    public void getPurchaseFind(){ assertEquals(true, p.find("a"));}
+    @Test
+    public void getPurchaseGetQ(){ assertEquals(5, p.getQuintity());}
+    @Test
+    public void getPurchaseGetName(){ assertEquals("a", p.getName());}
    
 }
